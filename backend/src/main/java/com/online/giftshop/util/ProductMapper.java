d63f2157d8bdd38e46ba4f55f8f2a7d347ac752b
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-13 23:23:33 +0530

    Added mappers

---
+++ backend/src/main/java/com/online/giftshop/util/ProductMapper.java
@@ -0,0 +1,52 @@
+package com.online.giftshop.util;
+
+import com.online.giftshop.dto.ProductInfoDto;
+import com.online.giftshop.dto.ProductInfoRequest;
+import com.online.giftshop.dto.StockDto;
+import com.online.giftshop.entities.ProductInfo;
+import com.online.giftshop.enums.ProductStatus;
+
+public class ProductMapper {
+	
+	public static ProductInfo DtoToEntity(ProductInfoRequest request) {
+		ProductInfo productInfo = new ProductInfo();
+		productInfo.setProductId(request.getProductId());
+		productInfo.setProductName(request.getProductName());
+		productInfo.setProductPrice(request.getProductPrice());
+		productInfo.setProductStock(Integer.valueOf(request.getProductStock()));
+		productInfo.setProductDescription(request.getProductDescription());
+		productInfo.setProductIcon(request.getProductIcon());
+		productInfo.setProductStatus(ProductStatus.valueOf(request.getProductStatus()));
+		productInfo.setDiscountPercent(request.getDiscountPercent());
+		return productInfo;
+	}
+	
+	public static ProductInfo stockDtoToEntity(StockDto stockDto) {
+		ProductInfo productInfo = new ProductInfo();
+		productInfo.setProductId(stockDto.getProductId());
+		productInfo.setProductStock(stockDto.getQuantity());
+//		productInfo.setProductName(productInfo.getProductName());
+//		productInfo.setProductDescription(productInfo.getProductDescription());
+//		productInfo.setProductIcon(productInfo.getProductIcon());
+//		productInfo.setProductPrice(productInfo.getProductPrice());
+//		productInfo.setProductStatus(productInfo.getProductStatus());
+//		productInfo.setProductCategory(productInfo.getProductCategory());
+		return productInfo;
+	}
+
+	public static ProductInfoDto EntityToDto(ProductInfo info) {
+		return ProductInfoDto.builder()
+			.categoryId(info.getProductCategory().getCategoryId())
+			.categoryName(info.getProductCategory().getCategoryName())
+			.productDescription(info.getProductDescription())
+			.productIcon(info.getProductIcon())
+			.productPrice(info.getProductPrice())
+			.productStock(info.getProductStock())
+			.productStatus(info.getProductStatus())
+			.productName(info.getProductName())
+			.productId(info.getProductId())
+			.discountPercent(info.getDiscountPercent())
+			.build();
+	}
+
+}
