b072cabc1be6790021936fcefe536b0ccc7063fe
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-13 22:39:11 +0530

    Scaffold backend

---
+++ backend/src/main/java/com/online/giftshop/GiftShopApplication.java
@@ -0,0 +1,13 @@
+package com.online.giftshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftShopApplication.class, args);
+	}
+
+}
