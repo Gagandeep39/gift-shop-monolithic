9213de8650f457721202cbff8f3711783d83b892
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-14 23:46:12 +0530

    Added support for CICD in frontend

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -39,6 +39,10 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    coverageIstanbulReporter: {
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+  },
   });
 };
