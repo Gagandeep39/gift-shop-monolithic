cca10c35d4cd6d7cdf2b55b0d02fc72e0d7ce0e5
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-03-04 19:48:44 +0530

    Added more environments

---
+++ frontend/src/environments/environment.aws.ts
@@ -0,0 +1,20 @@
+// http://giftshopapi-env.eba-gpuj7mei.us-east-2.elasticbeanstalk.com/
+export const environment = {
+  production: false,
+  name: 'aws',
+  protocol: 'http://',
+  applicationUrl: 'giftshopapi-env.eba-gpuj7mei.us-east-2.elasticbeanstalk.com',
+  // applicationUrl: 'localhost:1999',
+  authService: '',
+  productService: '',
+  orderService: '',
+  paymentService: '',
+  cartService: '',
+  mapQuestAPIKey: 'nXLMbpX6U0545mAaAJyeI2QQd1JYVqG4',
+  mapQuestUrl: 'http://www.mapquestapi.com/directions/v2/route',
+  publicStripeKey: 'pk_test_51I3zWOI8py6Rj5ufY6RnJ1zjPK930I2xwWW0XKPWFr6cp7mAvgM8q8aoAV5R0trqmGEEqSgrLY6xVhZRqTsgZJqE00zW2TqkZa',
+  publicRazorpayKey: 'rzp_test_QHK5P7ne66SdWo',
+  googleClientId: '120787335948-g4r6bof29ub3vaaaiu7pc01jd5dalooq.apps.googleusercontent.com',
+  facebookClientId: '745534413011559',
+  from: '400072',
+};
---
+++ frontend/src/environments/environment.docker.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  name: 'aws',
+  protocol: 'http://',
+  applicationUrl: 'localhost:1999',
+  authService: '',
+  productService: '',
+  orderService: '',
+  paymentService: '',
+  cartService: '',
+  mapQuestAPIKey: 'nXLMbpX6U0545mAaAJyeI2QQd1JYVqG4',
+  mapQuestUrl: 'http://www.mapquestapi.com/directions/v2/route',
+  publicStripeKey: 'pk_test_51I3zWOI8py6Rj5ufY6RnJ1zjPK930I2xwWW0XKPWFr6cp7mAvgM8q8aoAV5R0trqmGEEqSgrLY6xVhZRqTsgZJqE00zW2TqkZa',
+  publicRazorpayKey: 'rzp_test_QHK5P7ne66SdWo',
+  googleClientId: '120787335948-g4r6bof29ub3vaaaiu7pc01jd5dalooq.apps.googleusercontent.com',
+  facebookClientId: '745534413011559',
+  from: '400072',
+};
