66910d179ce903f4b5d4dceb086b6a244f164542
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-26 12:33:40 +0530

    Added social signin

--- frontend/src/app/components/auth/login/login.component.ts
+++ frontend/src/app/components/auth/login/login.component.ts
@@ -11,6 +11,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { LoadingService } from 'src/app/services/loading.service';
+import { SocialAuthService } from 'src/app/services/social-auth.service';
+import { environment } from 'src/environments/environment';
+
 declare const gapi: any;
 declare const FB: any;
 
@@ -31,6 +34,8 @@ export class LoginComponent implements OnInit {
     private router: Router,
     public loadingService: LoadingService,
     private route: ActivatedRoute,
+    private socialAuthService: SocialAuthService,
+    private ngZone: NgZone
   ) {}
 
   ngOnDestroy(): void {
@@ -76,9 +81,107 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  ngAfterViewInit() {
+  // Google sign in
+  auth2;
+  logo = '/assets/images/btn_google_light_normal.svg';
+
+  public googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: environment.googleClientId,
+      });
+      this.attachSignin(document.getElementById('googleBtn'));
+    });
+  }
+  public attachSignin(element) {
+    this.auth2.attachClickHandler(element, {}, (googleUser) =>
+      this.ngZone.run(
+        () => {
+          console.log(googleUser);
+          this.loadingService.enableLoading();
+          this.socialAuthService
+            .validateGoogleTokenAndLogin({
+              token: googleUser.getAuthResponse().id_token,
+              email: googleUser.getBasicProfile().getEmail(),
+            })
+            .subscribe((res) => {
+              this.loadingService.disableLoading();
+              if (res['status'] !== 202)
+                this.router.navigateByUrl(this.returnUrl);
+              else {
+                this.router.navigate(['/socialregister'], {
+                  state: {
+                    data: { emailId: googleUser.getBasicProfile().getEmail() },
+                  },
+                });
+              }
+            }).closed;
+        },
+        (error) => console.log(error)
+      )
+    );
   }
 
+  ngAfterViewInit() {
+    this.googleInit();
+    this.facebookInit();
+  }
+
+  facebookInit() {
+    (window as any).fbAsyncInit = function () {
+      FB.init({
+        appId: environment.facebookClientId,
+        cookie: true,
+        xfbml: true,
+        version: 'v3.1',
+      });
+      FB.AppEvents.logPageView();
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  }
+
+  submitLogin() {
+    FB.login((response) =>
+      this.ngZone.run(() => {
+        if (response.authResponse) {
+          FB.api('/me', { fields: 'name, email' }, (res) => {
+            console.log(res);
+            console.log(response);
+
+            this.loadingService.enableLoading();
+            this.socialAuthService
+              .validateFacebookTokenAndLogin({
+                token: response.authResponse.accessToken,
+                email: res.email,
+              })
+              .subscribe((res) => {
+                this.loadingService.disableLoading();
+                if (res['status'] !== 202)
+                  this.router.navigateByUrl(this.returnUrl);
+                else {
+                  this.router.navigate(['/socialregister'], {
+                    state: {
+                      data: { emailId: response.email },
+                    },
+                  });
+                }
+              }).closed;
+          });
+        } else throw Error('Error Signing from facebook');
+      })
+    );
+  }
 }
 
 /**
