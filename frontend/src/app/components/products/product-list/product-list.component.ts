91b5a829cd4d86c70e5d170870cdd70d391ba967
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-26 12:31:44 +0530

    Added pagination Added sorting Added carausel

--- frontend/src/app/components/products/product-list/product-list.component.ts
+++ frontend/src/app/components/products/product-list/product-list.component.ts
@@ -9,7 +9,6 @@ import { HostListener } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { take } from 'rxjs/operators';
-import { Item } from 'src/app/models/item.model';
 import { Product } from 'src/app/models/product.model';
 import { AuthModalService } from 'src/app/services/auth-modal.service';
 import { AuthService } from 'src/app/services/auth.service';
@@ -29,6 +28,18 @@ export class ProductListComponent implements OnInit {
   activeCategory = null;
   productQuery = null;
   page = 0;
+  sortByTypes = [
+    { name: 'Name - ASC', sortBy: 'productName', direction: 'ASC' },
+    { name: 'Name - DESC', sortBy: 'productName', direction: 'DESC' },
+    { name: 'Category - ASC', sortBy: 'productCategory_categoryName', direction: 'ASC' },
+    { name: 'Category - DESC', sortBy: 'productCategory_categoryName', direction: 'DESC' },
+    { name: 'Price High - Low', sortBy: 'productPrice', direction: 'DESC' },
+    { name: 'Price Low - High', sortBy: 'productPrice', direction: 'ASC' },
+    { name: 'Discount High - Low', sortBy: 'discountPercent', direction: 'DESC' },
+    { name: 'Discount Low - High', sortBy: 'discountPercent', direction: 'ASC' },
+  ];
+  activeSortType = this.sortByTypes[0];
+  sortTypeChanged;
 
   constructor(
     public loadingService: LoadingService,
@@ -42,13 +53,14 @@ export class ProductListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.page = 0;
     this.initProducts();
     this.subscribeToCategories();
     this.subscribeToSearchQuery();
   }
 
   initProducts() {
-    this.fetchAll();
+    this.fetchAllByPage();
   }
 
   // Fetch from server
@@ -62,13 +74,14 @@ export class ProductListComponent implements OnInit {
 
   // Reset all search queries
   resetAll() {
+    this.activeSortType = this.sortByTypes[0];
     this.eventService.categoryChanged.next(null);
     this.eventService.searchQueryChanged.next(null);
     this.productQuery = null;
     this.productList = null;
     this.activeCategory = null;
     this.page = 0;
-    this.fetchAll();
+    this.fetchAllByPage();
   }
 
   addToCart(itemId) {
@@ -96,15 +109,15 @@ export class ProductListComponent implements OnInit {
       this.loadingService.enableLoading();
       // Fetch and unsubscribe
       if (!query) {
-        this.page = 0;
-        this.fetchAll();
+        // this.page = 0;
+        // this.fetchAllByPage();
       } else
         this.productService.findByName(query).subscribe((res: Product[]) => {
           this.productQuery = query;
           this.activeCategory = null;
           this.productList = res;
           this.loadingService.disableLoading();
-        }).closed;
+        });
     });
   }
 
@@ -116,30 +129,72 @@ export class ProductListComponent implements OnInit {
       this.activeCategory = null;
       this.productQuery = null;
       if (!category) {
-        this.page = 0;
-        this.fetchAll();
+        // this.page = 0;
+        // this.fetchAllByPage();
       } else
         this.productService
           .findByCategory(category)
+          .pipe(take(1))
           .subscribe((res: Product[]) => {
             this.productQuery = null;
             this.activeCategory = category;
             this.productList = res;
             this.loadingService.disableLoading();
-          }).closed;
+          });
     });
   }
 
-  // fetchAllByPage() {
-  //   this.loadingService.enableLoading();
-  //   this.productService
-  //     .fetchAllProducts()
-  //     .pipe(take(1))
-  //     .subscribe((res: Product[]) => {
-  //       if (!this.activeCategory && !this.productQuery) // Paging works when category and search are null
-  //           this.productList.push(...res);
-  //         else this.productList = res;
-  //       this.loadingService.disableLoading();
-  //     });
-  // }
+  previousScrollValue = 0;
+
+  @HostListener('window:scroll', ['$event.target']) // for window scroll events
+  onScroll(event) {
+    if (
+      window.innerHeight + window.scrollY >= (document.body.scrollHeight-100) &&
+      !this.activeCategory &&
+      !this.productQuery &&
+      this.previousScrollValue !== window.innerHeight + window.scrollY
+    ) {
+      this.previousScrollValue = window.innerHeight + window.scrollY;
+      console.log(window.innerHeight + window.scrollY);
+      console.log(this.page);
+      
+      this.page++;
+      this.fetchAllByPage();
+    }
+  }
+
+  fetchAllByPage() {
+    this.loadingService.enableLoading();
+    this.productService
+      .fetchAllByPaging(this.page, this.activeSortType.sortBy, this.activeSortType.direction)
+      // .pipe(take(1))
+      .subscribe((res: Product[]) => {
+        console.log(this.page);
+        
+        if (!this.activeCategory && !this.productQuery)
+          if (
+            this.productList &&
+            JSON.stringify(
+              this.productList.slice(Math.max(this.productList.length - 10, 0))
+            ) != JSON.stringify(res) &&
+            !this.sortTypeChanged
+          )
+            // Paging works when category and search are null
+            this.productList.push(...res);
+          else {
+            this.sortTypeChanged = undefined;
+            this.productList = res;
+          }
+        this.loadingService.disableLoading();
+        console.log(this.productList);
+        
+      });
+  }
+
+  onChangeSorting(item) {
+    this.sortTypeChanged = 1;
+    this.activeSortType = item;
+    this.page = 0;
+    this.fetchAllByPage();
+  }
 }
