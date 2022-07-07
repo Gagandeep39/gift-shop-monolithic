f975e0b1b50569fa2c23153d2f4019ad70457219
Gagandeep39 <singh.gagandeep3911@gmail.com>
2022-07-07 13:20:00 +0000

    Deploying to gh-pages from @ Gagandeep39/gift-shop-monolithic@4d75d23048a755617dad28813db87b3e1ca517e6 🚀

--- main.js
+++ main.js
@@ -11535,35 +11535,23 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "environment": () => (/* binding */ environment)
 /* harmony export */ });
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    protocol: 'http://',
-    applicationUrl: 'localhost:8085/gift-shop-api',
-    // applicationUrl: 'localhost:1999',
+    protocol: 'https://',
+    applicationUrl: 'glacial-bastion-78267.herokuapp.com',
     authService: '',
     productService: '',
     orderService: '',
     paymentService: '',
     cartService: '',
     mapQuestAPIKey: 'nXLMbpX6U0545mAaAJyeI2QQd1JYVqG4',
-    mapQuestUrl: 'http://www.mapquestapi.com/directions/v2/route',
+    mapQuestUrl: 'https://www.mapquestapi.com/directions/v2/route',
     publicStripeKey: 'pk_test_51I3zWOI8py6Rj5ufY6RnJ1zjPK930I2xwWW0XKPWFr6cp7mAvgM8q8aoAV5R0trqmGEEqSgrLY6xVhZRqTsgZJqE00zW2TqkZa',
     publicRazorpayKey: 'rzp_test_QHK5P7ne66SdWo',
     googleClientId: '120787335948-g4r6bof29ub3vaaaiu7pc01jd5dalooq.apps.googleusercontent.com',
     facebookClientId: '745534413011559',
     from: '400072',
 };
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
 
 
 /***/ }),
