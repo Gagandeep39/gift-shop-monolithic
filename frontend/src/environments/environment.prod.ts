24fc8f238e83d775d1bf1d4535e40b900f731312
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2022-07-07 17:43:57 +0530

    Added https for mapquest

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   protocol: 'https://',
-  // applicationUrl: 'localhost:8085/gift-shop-api',
   applicationUrl: 'glacial-bastion-78267.herokuapp.com',
   authService: '',
   productService: '',
@@ -9,10 +8,12 @@ export const environment = {
   paymentService: '',
   cartService: '',
   mapQuestAPIKey: 'nXLMbpX6U0545mAaAJyeI2QQd1JYVqG4',
-  mapQuestUrl: 'http://www.mapquestapi.com/directions/v2/route',
-  publicStripeKey: 'pk_test_51I3zWOI8py6Rj5ufY6RnJ1zjPK930I2xwWW0XKPWFr6cp7mAvgM8q8aoAV5R0trqmGEEqSgrLY6xVhZRqTsgZJqE00zW2TqkZa',
+  mapQuestUrl: 'https://www.mapquestapi.com/directions/v2/route',
+  publicStripeKey:
+    'pk_test_51I3zWOI8py6Rj5ufY6RnJ1zjPK930I2xwWW0XKPWFr6cp7mAvgM8q8aoAV5R0trqmGEEqSgrLY6xVhZRqTsgZJqE00zW2TqkZa',
   publicRazorpayKey: 'rzp_test_QHK5P7ne66SdWo',
-  googleClientId: '120787335948-g4r6bof29ub3vaaaiu7pc01jd5dalooq.apps.googleusercontent.com',
+  googleClientId:
+    '120787335948-g4r6bof29ub3vaaaiu7pc01jd5dalooq.apps.googleusercontent.com',
   facebookClientId: '745534413011559',
   from: '400072',
 };
