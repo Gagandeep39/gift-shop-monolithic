455d02e41c4086a7f7c76143e0ca2f1a03753e49
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2023-03-08 14:20:29 +0530

    Removed unused components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,12 +9,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialCustomModule } from './material-custom/material-custom.module';
 import { httpInterceptors } from './providers';
 import { SharedModule } from './shared/shared.module';
 
@@ -26,10 +24,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
     HttpClientModule,
-    FormsModule,
-    MaterialCustomModule,
     SharedModule
   ],
   providers: [
